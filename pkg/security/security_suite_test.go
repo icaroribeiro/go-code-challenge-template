1e48e779c9a248915479e0e6fa754c42c5a1a2d3
Ícaro <icaroribeiro@hotmail.com>
2022-02-17 22:22:45 -0300

    dev

---
+++ pkg/security/security_suite_test.go
@@ -0,0 +1,22 @@
+package security_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type Case struct {
+	Context   string
+	SetUp     func(t *testing.T)
+	WantError bool
+}
+
+type Cases []Case
+
+type ReturnArgs [][]interface{}
+
+type TestSuite struct {
+	suite.Suite
+	Cases Cases
+}
