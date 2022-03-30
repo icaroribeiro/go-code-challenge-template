c2b24a1ec757b47cd6378d2920886e5267d92b3b
Ícaro <icaroribeiro@hotmail.com>
2022-03-30 19:42:38 -0300

    dev

--- internal/infrastructure/storage/datastore/repository/login/repository_test.go
+++ internal/infrastructure/storage/datastore/repository/login/repository_test.go
@@ -380,114 +380,106 @@ func (ts *TestSuite) TestUpdate() {
 			},
 			WantError: false,
 		},
-		// {
-		// 	Context: "ItShouldFailIfAnErrorOccursWhenUpdatingTheLogin",
-		// 	SetUp: func(t *testing.T) {
-		// 		id = uuid.NewV4()
+		{
+			Context: "ItShouldFailIfAnErrorOccursWhenUpdatingTheLogin",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-		// 		args := map[string]interface{}{
-		// 			"id":        uuid.Nil,
-		// 			"createdAt": time.Time{},
-		// 			"updatedAt": time.Time{},
-		// 		}
+				args := map[string]interface{}{
+					"id": uuid.Nil,
+				}
 
-		// 		login = logindbmodelfactory.New(args)
+				login = domainfactorymodel.NewLogin(args)
 
-		// 		mock.ExpectBegin()
+				mock.ExpectBegin()
 
-		// 		mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
-		// 			WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
-		// 			WillReturnError(customerror.New("failed"))
+				mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
+					WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
+					WillReturnError(customerror.New("failed"))
 
-		// 		mock.ExpectRollback()
+				mock.ExpectRollback()
 
-		// 		errorType = customerror.NoType
-		// 	},
-		// 	WantError: true,
-		// },
-		// {
-		// 	Context: "ItShouldFailIfTheLoginIsNotFound",
-		// 	SetUp: func(t *testing.T) {
-		// 		id = uuid.NewV4()
+				errorType = customerror.NoType
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfTheLoginIsNotFound",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-		// 		args := map[string]interface{}{
-		// 			"id":        uuid.Nil,
-		// 			"createdAt": time.Time{},
-		// 			"updatedAt": time.Time{},
-		// 		}
+				args := map[string]interface{}{
+					"id": uuid.Nil,
+				}
 
-		// 		login = logindbmodelfactory.New(args)
+				login = domainfactorymodel.NewLogin(args)
 
-		// 		mock.ExpectBegin()
+				mock.ExpectBegin()
 
-		// 		mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
-		// 			WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
-		// 			WillReturnResult(sqlmock.NewResult(0, 0))
+				mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
+					WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
+					WillReturnResult(sqlmock.NewResult(0, 0))
 
-		// 		mock.ExpectCommit()
+				mock.ExpectCommit()
 
-		// 		errorType = customerror.NotFound
-		// 	},
-		// 	WantError: true,
-		// },
-		// {
-		// 	Context: "ItShouldFailIfAnErrorOccursWhenFindingTheLoginByID",
-		// 	SetUp: func(t *testing.T) {
-		// 		id = uuid.NewV4()
+				errorType = customerror.NotFound
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfAnErrorOccursWhenFindingTheLoginByID",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-		// 		args := map[string]interface{}{
-		// 			"id":        uuid.Nil,
-		// 			"createdAt": time.Time{},
-		// 			"updatedAt": time.Time{},
-		// 		}
+				args := map[string]interface{}{
+					"id": uuid.Nil,
+				}
 
-		// 		login = logindbmodelfactory.New(args)
+				login = domainfactorymodel.NewLogin(args)
 
-		// 		mock.ExpectBegin()
+				mock.ExpectBegin()
 
-		// 		mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
-		// 			WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
-		// 			WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
+					WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
+					WillReturnResult(sqlmock.NewResult(0, 1))
 
-		// 		mock.ExpectCommit()
+				mock.ExpectCommit()
 
-		// 		mock.ExpectQuery(regexp.QuoteMeta(secondStmt)).
-		// 			WithArgs(id).
-		// 			WillReturnError(customerror.New("failed"))
+				mock.ExpectQuery(regexp.QuoteMeta(secondStmt)).
+					WithArgs(id).
+					WillReturnError(customerror.New("failed"))
 
-		// 		errorType = customerror.NoType
-		// 	},
-		// 	WantError: true,
-		// },
-		// {
-		// 	Context: "ItShouldFailIfTheLoginIsNotFoundAfterUpdatingIt",
-		// 	SetUp: func(t *testing.T) {
-		// 		id = uuid.NewV4()
+				errorType = customerror.NoType
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfTheLoginIsNotFoundAfterUpdatingIt",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-		// 		args := map[string]interface{}{
-		// 			"id":        uuid.Nil,
-		// 			"createdAt": time.Time{},
-		// 			"updatedAt": time.Time{},
-		// 		}
+				args := map[string]interface{}{
+					"id": uuid.Nil,
+				}
 
-		// 		login = logindbmodelfactory.New(args)
+				login = domainfactorymodel.NewLogin(args)
 
-		// 		mock.ExpectBegin()
+				mock.ExpectBegin()
 
-		// 		mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
-		// 			WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
-		// 			WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec(regexp.QuoteMeta(firstStmt)).
+					WithArgs(login.UserID, login.Username, sqlmock.AnyArg(), sqlmock.AnyArg(), id).
+					WillReturnResult(sqlmock.NewResult(0, 1))
 
-		// 		mock.ExpectCommit()
+				mock.ExpectCommit()
 
-		// 		mock.ExpectQuery(regexp.QuoteMeta(secondStmt)).
-		// 			WithArgs(id).
-		// 			WillReturnRows(&sqlmock.Rows{})
+				mock.ExpectQuery(regexp.QuoteMeta(secondStmt)).
+					WithArgs(id).
+					WillReturnRows(&sqlmock.Rows{})
 
-		// 		errorType = customerror.NotFound
-		// 	},
-		// 	WantError: true,
-		// },
+				errorType = customerror.NotFound
+			},
+			WantError: true,
+		},
 	}
 
 	for _, tc := range ts.Cases {
@@ -516,159 +508,165 @@ func (ts *TestSuite) TestUpdate() {
 	}
 }
 
-// func (ts *TestSuite) TestDelete() {
-// 	var id uuid.UUID
+func (ts *TestSuite) TestDelete() {
+	driver := "postgres"
+	db, mock := NewMock(driver)
 
-// 	var login logindbmodel.Login
+	var id uuid.UUID
 
-// 	errorType := customerror.NoType
+	login := domainmodel.Login{}
 
-// 	firstStmt := `SELECT * FROM "logins" WHERE id=$1`
+	errorType := customerror.NoType
 
-// 	secondStmt := `DELETE FROM "logins" WHERE "logins"."id" = $1`
+	firstStmt := `SELECT * FROM "logins" WHERE id=$1`
 
-// 	ts.Cases = Cases{
-// 		{
-// 			Context: "ItShouldSucceedInDeletingTheLogin",
-// 			SetUp: func(t *testing.T) {
-// 				id = uuid.NewV4()
+	secondStmt := `DELETE FROM "logins" WHERE "logins"."id" = $1`
 
-// 				args := map[string]interface{}{
-// 					"id": id,
-// 				}
+	ts.Cases = Cases{
+		{
+			Context: "ItShouldSucceedInDeletingTheLogin",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-// 				login = logindbmodelfactory.New(args)
+				args := map[string]interface{}{
+					"id": id,
+				}
 
-// 				rows := sqlmock.
-// 					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
-// 					AddRow(login.ID, login.UserID, login.Username, login.Password, login.CreatedAt, login.UpdatedAt)
+				datastoreLogin := datastorefactorymodel.NewLogin(args)
+				login = datastoreLogin.ToDomain()
 
-// 				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
-// 					WithArgs(id).
-// 					WillReturnRows(rows)
+				rows := sqlmock.
+					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
+					AddRow(datastoreLogin.ID, datastoreLogin.UserID, datastoreLogin.Username, datastoreLogin.Password, datastoreLogin.CreatedAt, datastoreLogin.UpdatedAt)
 
-// 				mock.ExpectBegin()
+				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
+					WithArgs(id).
+					WillReturnRows(rows)
 
-// 				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
-// 					WithArgs(id).
-// 					WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectBegin()
 
-// 				mock.ExpectCommit()
-// 			},
-// 			WantError: false,
-// 		},
-// 		{
-// 			Context: "ItShouldFailIfAnErrorOccursWhenFindingTheLoginByID",
-// 			SetUp: func(t *testing.T) {
-// 				id = uuid.NewV4()
+				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
+					WithArgs(id).
+					WillReturnResult(sqlmock.NewResult(0, 1))
 
-// 				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
-// 					WithArgs(id).
-// 					WillReturnError(customerror.New("failed"))
+				mock.ExpectCommit()
+			},
+			WantError: false,
+		},
+		{
+			Context: "ItShouldFailIfAnErrorOccursWhenFindingTheLoginByID",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-// 				errorType = customerror.NoType
-// 			},
-// 			WantError: true,
-// 		},
-// 		{
-// 			Context: "ItShouldFailIfTheLoginIsNotFound",
-// 			SetUp: func(t *testing.T) {
-// 				id = uuid.NewV4()
+				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
+					WithArgs(id).
+					WillReturnError(customerror.New("failed"))
 
-// 				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
-// 					WithArgs(id).
-// 					WillReturnRows(&sqlmock.Rows{})
+				errorType = customerror.NoType
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfTheLoginIsNotFound",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-// 				errorType = customerror.NotFound
-// 			},
-// 			WantError: true,
-// 		},
-// 		{
-// 			Context: "ItShouldFailIfAnErrorOccursWhenDeletingTheLoginByID",
-// 			SetUp: func(t *testing.T) {
-// 				id = uuid.NewV4()
+				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
+					WithArgs(id).
+					WillReturnRows(&sqlmock.Rows{})
 
-// 				args := map[string]interface{}{
-// 					"id": id,
-// 				}
-// 				login = logindbmodelfactory.New(args)
+				errorType = customerror.NotFound
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfAnErrorOccursWhenDeletingTheLoginByID",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-// 				rows := sqlmock.
-// 					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
-// 					AddRow(login.ID, login.UserID, login.Username, login.Password, login.CreatedAt, login.UpdatedAt)
+				args := map[string]interface{}{
+					"id": id,
+				}
 
-// 				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
-// 					WithArgs(id).
-// 					WillReturnRows(rows)
+				datastoreLogin := datastorefactorymodel.NewLogin(args)
+				login = datastoreLogin.ToDomain()
 
-// 				mock.ExpectBegin()
+				rows := sqlmock.
+					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
+					AddRow(datastoreLogin.ID, datastoreLogin.UserID, datastoreLogin.Username, datastoreLogin.Password, datastoreLogin.CreatedAt, datastoreLogin.UpdatedAt)
 
-// 				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
-// 					WithArgs(id).
-// 					WillReturnError(customerror.New("failed"))
+				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
+					WithArgs(id).
+					WillReturnRows(rows)
 
-// 				mock.ExpectRollback()
+				mock.ExpectBegin()
 
-// 				errorType = customerror.NoType
-// 			},
-// 			WantError: true,
-// 		},
-// 		{
-// 			Context: "ItShouldFailIfTheLoginIsNotDeleted",
-// 			SetUp: func(t *testing.T) {
-// 				id = uuid.NewV4()
+				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
+					WithArgs(id).
+					WillReturnError(customerror.New("failed"))
 
-// 				args := map[string]interface{}{
-// 					"id": id,
-// 				}
-// 				login = logindbmodelfactory.New(args)
+				mock.ExpectRollback()
 
-// 				rows := sqlmock.
-// 					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
-// 					AddRow(login.ID, login.UserID, login.Username, login.Password, login.CreatedAt, login.UpdatedAt)
+				errorType = customerror.NoType
+			},
+			WantError: true,
+		},
+		{
+			Context: "ItShouldFailIfTheLoginIsNotDeleted",
+			SetUp: func(t *testing.T) {
+				id = uuid.NewV4()
 
-// 				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
-// 					WithArgs(id).
-// 					WillReturnRows(rows)
+				args := map[string]interface{}{
+					"id": id,
+				}
 
-// 				mock.ExpectBegin()
+				datastoreLogin := datastorefactorymodel.NewLogin(args)
+				login = datastoreLogin.ToDomain()
 
-// 				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
-// 					WithArgs(id).
-// 					WillReturnResult(sqlmock.NewResult(0, 0))
+				rows := sqlmock.
+					NewRows([]string{"id", "user_id", "username", "password", "created_at", "updated_at"}).
+					AddRow(datastoreLogin.ID, datastoreLogin.UserID, datastoreLogin.Username, datastoreLogin.Password, datastoreLogin.CreatedAt, datastoreLogin.UpdatedAt)
 
-// 				mock.ExpectCommit()
+				mock.ExpectQuery(regexp.QuoteMeta(firstStmt)).
+					WithArgs(id).
+					WillReturnRows(rows)
 
-// 				errorType = customerror.NotFound
-// 			},
-// 			WantError: true,
-// 		},
-// 	}
+				mock.ExpectBegin()
 
-// 	for _, tc := range ts.Cases {
-// 		ts.T().Run(tc.Context, func(t *testing.T) {
-// 			tc.SetUp(t)
+				mock.ExpectExec(regexp.QuoteMeta(secondStmt)).
+					WithArgs(id).
+					WillReturnResult(sqlmock.NewResult(0, 0))
 
-// 			loginDatastoreRepository := logindbrepository.New(ts.DB)
+				mock.ExpectCommit()
 
-// 			returnedLogin, err := loginDatastoreRepository.Delete(id.String())
+				errorType = customerror.NotFound
+			},
+			WantError: true,
+		},
+	}
 
-// 			if !tc.WantError {
-// 				assert.Nil(t, err, fmt.Sprintf("Unexpected error %v.", err))
-// 				assert.Equal(t, login.ID, returnedLogin.ID)
-// 				assert.Equal(t, login.UserID, returnedLogin.UserID)
-// 				assert.Equal(t, login.Username, returnedLogin.Username)
-// 				assert.Equal(t, login.Password, returnedLogin.Password)
-// 				assert.Equal(t, login.CreatedAt, returnedLogin.CreatedAt)
-// 				assert.Equal(t, login.UpdatedAt, returnedLogin.UpdatedAt)
-// 			} else {
-// 				assert.NotNil(t, err, "Predicted error lost.")
-// 				assert.Equal(t, errorType, customerror.GetType(err))
-// 				assert.Empty(t, returnedLogin)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tc := range ts.Cases {
+		ts.T().Run(tc.Context, func(t *testing.T) {
+			tc.SetUp(t)
+
+			loginDatastoreRepository := logindatastorerepository.New(db)
+
+			returnedLogin, err := loginDatastoreRepository.Delete(id.String())
+
+			if !tc.WantError {
+				assert.Nil(t, err, fmt.Sprintf("Unexpected error %v.", err))
+				assert.Equal(t, login.ID, returnedLogin.ID)
+				assert.Equal(t, login.UserID, returnedLogin.UserID)
+				assert.Equal(t, login.Username, returnedLogin.Username)
+				assert.Equal(t, login.Password, returnedLogin.Password)
+			} else {
+				assert.NotNil(t, err, "Predicted error lost.")
+				assert.Equal(t, errorType, customerror.GetType(err))
+				assert.Empty(t, returnedLogin)
+			}
+		})
+	}
+}
 
 func (ts *TestSuite) TestWithDBTrx() {
 	driver := "postgres"
