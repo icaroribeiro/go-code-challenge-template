d492974c81ed878844f3fe49f66733f0df6b13c4
Ícaro <icaroribeiro@hotmail.com>
2022-03-31 20:30:10 -0300

    dev

--- internal/application/service/user/service_test.go
+++ internal/application/service/user/service_test.go
@@ -1,77 +1,77 @@
 package user_test
 
-import (
-	"fmt"
-	"testing"
+// import (
+// 	"fmt"
+// 	"testing"
 
-	userservice "github.com/icaroribeiro/new-go-code-challenge-template/internal/application/service/user"
-	domainmodel "github.com/icaroribeiro/new-go-code-challenge-template/internal/core/domain/model"
-	userdatastoremockrepository "github.com/icaroribeiro/new-go-code-challenge-template/internal/core/ports/infrastructure/storage/datastore/mockrepository/user"
-	"github.com/icaroribeiro/new-go-code-challenge-template/pkg/customerror"
-	domainfactorymodel "github.com/icaroribeiro/new-go-code-challenge-template/tests/factory/core/domain/model"
-	"github.com/icaroribeiro/new-go-code-challenge-template/tests/mocks/pkg/mockvalidator"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
-)
+// 	userservice "github.com/icaroribeiro/new-go-code-challenge-template/internal/application/service/user"
+// 	domainmodel "github.com/icaroribeiro/new-go-code-challenge-template/internal/core/domain/model"
+// 	userdatastoremockrepository "github.com/icaroribeiro/new-go-code-challenge-template/internal/core/ports/infrastructure/storage/datastore/mockrepository/user"
+// 	"github.com/icaroribeiro/new-go-code-challenge-template/pkg/customerror"
+// 	domainfactorymodel "github.com/icaroribeiro/new-go-code-challenge-template/tests/factory/core/domain/model"
+// 	"github.com/icaroribeiro/new-go-code-challenge-template/tests/mocks/pkg/mockvalidator"
+// 	"github.com/stretchr/testify/assert"
+// 	"github.com/stretchr/testify/suite"
+// )
 
-func TestService(t *testing.T) {
-	suite.Run(t, new(TestSuite))
-}
+// func TestService(t *testing.T) {
+// 	suite.Run(t, new(TestSuite))
+// }
 
-func (ts *TestSuite) TestGetAll() {
-	user := domainmodel.User{}
+// func (ts *TestSuite) TestGetAll() {
+// 	user := domainmodel.User{}
 
-	returnArgs := ReturnArgs{}
+// 	returnArgs := ReturnArgs{}
 
-	errorType := customerror.NoType
+// 	errorType := customerror.NoType
 
-	ts.Cases = Cases{
-		{
-			Context: "ItShouldSucceedInGettingAllUsers",
-			SetUp: func(t *testing.T) {
-				user = domainfactorymodel.NewUser(nil)
+// 	ts.Cases = Cases{
+// 		{
+// 			Context: "ItShouldSucceedInGettingAllUsers",
+// 			SetUp: func(t *testing.T) {
+// 				user = domainfactorymodel.NewUser(nil)
 
-				returnArgs = ReturnArgs{
-					{domainmodel.Users{user}, nil},
-				}
-			},
-			WantError: false,
-		},
-		{
-			Context: "ItShouldFailIfItIsNotPossibleToGetAllUsers",
-			SetUp: func(t *testing.T) {
-				returnArgs = ReturnArgs{
-					{domainmodel.Users{}, customerror.New("failed")},
-				}
+// 				returnArgs = ReturnArgs{
+// 					{domainmodel.Users{user}, nil},
+// 				}
+// 			},
+// 			WantError: false,
+// 		},
+// 		{
+// 			Context: "ItShouldFailIfItIsNotPossibleToGetAllUsers",
+// 			SetUp: func(t *testing.T) {
+// 				returnArgs = ReturnArgs{
+// 					{domainmodel.Users{}, customerror.New("failed")},
+// 				}
 
-				errorType = customerror.NoType
-			},
-			WantError: true,
-		},
-	}
+// 				errorType = customerror.NoType
+// 			},
+// 			WantError: true,
+// 		},
+// 	}
 
-	for _, tc := range ts.Cases {
-		ts.T().Run(tc.Context, func(t *testing.T) {
-			tc.SetUp(t)
+// 	for _, tc := range ts.Cases {
+// 		ts.T().Run(tc.Context, func(t *testing.T) {
+// 			tc.SetUp(t)
 
-			userDatastoreRepository := new(userdatastoremockrepository.Repository)
-			userDatastoreRepository.On("GetAll").Return(returnArgs[0]...)
+// 			userDatastoreRepository := new(userdatastoremockrepository.Repository)
+// 			userDatastoreRepository.On("GetAll").Return(returnArgs[0]...)
 
-			validator := new(mockvalidator.Validator)
+// 			validator := new(mockvalidator.Validator)
 
-			userService := userservice.New(userDatastoreRepository, validator)
+// 			userService := userservice.New(userDatastoreRepository, validator)
 
-			returnedUsers, err := userService.GetAll()
+// 			returnedUsers, err := userService.GetAll()
 
-			if !tc.WantError {
-				assert.Nil(t, err, fmt.Sprintf("Unexpected error %v.", err))
-				assert.Equal(t, user.ID, returnedUsers[0].ID)
-				assert.Equal(t, user.Username, returnedUsers[0].Username)
-			} else {
-				assert.NotNil(t, err, "Predicted error lost.")
-				assert.Equal(t, errorType, customerror.GetType(err))
-				assert.Empty(t, returnedUsers)
-			}
-		})
-	}
-}
+// 			if !tc.WantError {
+// 				assert.Nil(t, err, fmt.Sprintf("Unexpected error %v.", err))
+// 				assert.Equal(t, user.ID, returnedUsers[0].ID)
+// 				assert.Equal(t, user.Username, returnedUsers[0].Username)
+// 			} else {
+// 				assert.NotNil(t, err, "Predicted error lost.")
+// 				assert.Equal(t, errorType, customerror.GetType(err))
+// 				assert.Empty(t, returnedUsers)
+// 			}
+// 		})
+// 	}
+// }
