63c841825e2c067a47c35522c1bd4af5c6ab7aa7
Ícaro <icaroribeiro@hotmail.com>
2022-04-01 19:18:46 -0300

    dev

--- tests/api/user/user_suite_test.go
+++ tests/api/user/user_suite_test.go
@@ -4,10 +4,10 @@ import (
 	"log"
 	"testing"
 
-	"github.com/icaroribeiro/go-code-challenge-template/internal/infrastructure/env"
-	databasepkg "github.com/icaroribeiro/go-code-challenge-template/pkg/datastore"
-	validatorpkg "github.com/icaroribeiro/go-code-challenge-template/pkg/validator"
-	"github.com/icaroribeiro/go-code-challenge-template/pkg/validator/validationfunc"
+	datastorepkg "github.com/icaroribeiro/new-go-code-challenge-template/pkg/datastore"
+	envpkg "github.com/icaroribeiro/new-go-code-challenge-template/pkg/env"
+	validatorpkg "github.com/icaroribeiro/new-go-code-challenge-template/pkg/validator"
+	uuidvalidator "github.com/icaroribeiro/new-go-code-challenge-template/pkg/validator/uuid"
 	"github.com/stretchr/testify/suite"
 	validatorv2 "gopkg.in/validator.v2"
 	"gorm.io/gorm"
@@ -31,66 +31,64 @@ type TestSuite struct {
 }
 
 var (
-	dbDriver   = env.GetEnvWithDefaultValue("DB_DRIVER", "postgres")
-	dbUser     = env.GetEnvWithDefaultValue("DB_USER", "postgres")
-	dbPassword = env.GetEnvWithDefaultValue("DB_PASSWORD", "postgres")
-	dbHost     = env.GetEnvWithDefaultValue("DB_HOST", "localhost")
-	dbPort     = env.GetEnvWithDefaultValue("DB_PORT", "5432")
-	dbName     = env.GetEnvWithDefaultValue("DB_NAME", "testdb")
+	dbDriver   = envpkg.GetEnvWithDefaultValue("DB_DRIVER", "postgres")
+	dbUser     = envpkg.GetEnvWithDefaultValue("DB_USER", "postgres")
+	dbPassword = envpkg.GetEnvWithDefaultValue("DB_PASSWORD", "postgres")
+	dbHost     = envpkg.GetEnvWithDefaultValue("DB_HOST", "localhost")
+	dbPort     = envpkg.GetEnvWithDefaultValue("DB_PORT", "5432")
+	dbName     = envpkg.GetEnvWithDefaultValue("DB_NAME", "db")
 )
 
-func (ts *TestSuite) SetupSuite() {
-	dbConfig := setDBConfig()
+func setupDBConfig() (map[string]string, error) {
+	dbConfig := map[string]string{
+		"DRIVER":   dbDriver,
+		"USER":     dbUser,
+		"PASSWORD": dbPassword,
+		"HOST":     dbHost,
+		"PORT":     dbPort,
+		"NAME":     dbName,
+	}
 
-	database, err := databasepkg.New(dbConfig)
+	return dbConfig, nil
+}
+
+func (ts *TestSuite) SetupSuite() {
+	dbConfig, err := setupDBConfig()
 	if err != nil {
 		log.Panic(err.Error())
 	}
 
-	ts.DB = database.Instance
+	datastore, err := datastorepkg.New(dbConfig)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	ts.DB = datastore.GetInstance()
 	if ts.DB == nil {
-		log.Panicf("The database is null")
+		log.Panicf("The database instance is null")
 	}
 
 	if err = ts.DB.Error; err != nil {
-		log.Panicf("%s", err.Error())
+		log.Panicf("Got error when acessing the database instance: %s", err.Error())
 	}
 
-	validationFunc := validationfunc.New()
-
-	validationFuncMap := map[string]validatorv2.ValidationFunc{
-		"uuid":     validationFunc.ValidateUUID,
-		"name":     validationFunc.ValidateName,
-		"username": validationFunc.ValidateUsername,
-		"password": validationFunc.ValidatePassword,
+	validationFuncs := map[string]validatorv2.ValidationFunc{
+		"uuid": uuidvalidator.Validate,
 	}
 
-	ts.Validator, err = validatorpkg.New(validationFuncMap)
+	ts.Validator, err = validatorpkg.New(validationFuncs)
 	if err != nil {
-		log.Panicf("%s", err.Error())
+		log.Panicf("Got error when setting up the validator: %s", err.Error())
 	}
 }
 
 func (ts *TestSuite) TearDownSuite() {
 	db, err := ts.DB.DB()
 	if err != nil {
-		log.Panicf("%s", err.Error())
+		log.Panicf("Got error when acessing *sql.DB from database instance: %s", err.Error())
 	}
 
 	if err = db.Close(); err != nil {
-		log.Panicf("%s", err.Error())
+		log.Panicf("Got error when closing the database instance: %s", err.Error())
 	}
-}
-
-func setDBConfig() map[string]string {
-	dbConfig := map[string]string{
-		"driver":   dbDriver,
-		"user":     dbUser,
-		"password": dbPassword,
-		"host":     dbHost,
-		"port":     dbPort,
-		"name":     dbName,
-	}
-
-	return dbConfig
 }
