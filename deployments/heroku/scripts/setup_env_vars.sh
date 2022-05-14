c298d588eb8b3c125ad9c647b2b1b7672c64163c
Ícaro <icaroribeiro@hotmail.com>
2022-05-13 23:23:07 -0300

    WIP

--- deployments/heroku/scripts/setup_env_vars.sh
+++ deployments/heroku/scripts/setup_env_vars.sh
@@ -4,7 +4,7 @@
 # Heroku platform settings
 #
 export TF_VAR_heroku_email=icaroribeiro@hotmail.com
-export TF_VAR_heroku_api_key=1ef4bd50-ae60-437c-a7bc-a5ecf23fffd4
+export TF_VAR_heroku_api_key=f46318f1-e281-4aac-a95a-48388d0ef957
 
 #
 # Heroku application settings
